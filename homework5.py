db3632f9940b546ed265e3c7607a289b53210f2b
igor0073 <166950497+igor0073@users.noreply.github.com>
2024-05-08 19:24:56 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+def print_params(speed, time):
+    print('speed =', speed, 'time =  ', time)
+
+print_params('160 km/h',  '1 hour')
+print_params('200 km/h', '1 hour')
+print_params('250 km/h', '1 hour')
